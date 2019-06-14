066ed3cdeb011da9eacd6cda6c9ebd0c3ac04ec6
whitcompcsp <50707156+whitcompcsp@users.noreply.github.com>
2019-06-13 20:42:37 -0500

    Implement superclasses for Character, Survivor, and Zombie

---
+++ src/zombieuniversity/Zombie.java
@@ -0,0 +1,12 @@
+package zombieuniversity;
+
+public abstract class Zombie extends Character {
+    /**
+     * Initialize a zombie
+     * @param maxHealth maximum health of the character
+     * @param damage    damage of the character
+     */
+    protected Zombie(int maxHealth, int damage) {
+        super(maxHealth, damage);
+    }
+}
