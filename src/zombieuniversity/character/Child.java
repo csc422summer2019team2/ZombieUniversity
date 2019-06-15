2d5ce17d34fe05f2406fdad11ecf1816768021c1
Patricia (Jeudy) Obaseki <w.patricia.jeudy@gmail.com>
2019-06-14 23:02:09 -0500

    Added Child, Soldier, Teacher classes.

---
+++ src/zombieuniversity/character/Child.java
@@ -0,0 +1,12 @@
+package zombieuniversity.character;
+
+public class Child extends Survivor {         //inheriting the property of survivor for Child
+    public Child() {
+        super(20, 2) ;      //health 20 and attack power 2 damage
+    }
+    
+    @Override
+    public String toString() {
+        return "Child";
+    }
+}
