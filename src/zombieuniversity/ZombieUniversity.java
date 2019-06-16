ca157727643a2951cff93ff8ff348665bfee2063
whitcompcsp <50707156+whitcompcsp@users.noreply.github.com>
2019-06-16 00:26:51 -0500

    Count zombies and survivors

--- src/zombieuniversity/ZombieUniversity.java
+++ src/zombieuniversity/ZombieUniversity.java
@@ -5,8 +5,7 @@
  */
 package zombieuniversity;
 
-import zombieuniversity.character.Survivor;
-import zombieuniversity.character.Zombie;
+import zombieuniversity.character.*;
 
 public class ZombieUniversity {
 
@@ -17,8 +16,54 @@ public class ZombieUniversity {
 
         Survivor[] survivors = new CharacterFactory().randomSurvivorArray(0, 35);
         Zombie[] zombies = new CharacterFactory().randomZombieArray(0, 25);
-        System.out.println("We have " + survivors.length + " survivors trying to make it to safety.\n"
-                + "But there are " + zombies.length + " zombies waiting for them.");
+        
+        // Count the number of character types in each array
+        int teachers = 0, soldiers = 0, children = 0;
+        int commonInfected = 0, tanks = 0;
+        
+        for(Survivor s : survivors) {
+            if(s instanceof Child) {
+                children++;
+            }
+            else if(s instanceof Soldier) {
+                soldiers++;
+            }
+            else if(s instanceof Teacher) {
+                teachers++;
+            }
+        }
+        for(Zombie z : zombies) {
+            if(z instanceof CommonInfected) {
+                commonInfected++;
+            }
+            else if(z instanceof Tank) {
+                tanks++;
+            }
+        }
+        
+        // Output the number of survivors.
+        System.out.printf(
+                "We have %d survivor%s trying to make it to safety (%d child%s, %d teacher%s, %d soldier%s)\n",
+                survivors.length,
+                survivors.length == 1 ? "" : "s",
+                children,
+                children == 1 ? "" : "ren",
+                teachers,
+                teachers == 1 ? "" : "s",
+                soldiers,
+                soldiers == 1 ? "" : "s"
+        );
+        
+        // Output the number of zombies
+        System.out.printf(
+                "But there are %d zombie%s waiting for them (%d common infected, %d tank%s)\n",
+                zombies.length,
+                zombies.length == 1 ? "" : "s",
+                commonInfected,
+                tanks,
+                tanks == 1 ? "" : "s"
+        );
+        
         CharacterOrder master = new CharacterOrder();
         master.attackRotation(survivors, zombies);
 
